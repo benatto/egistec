d8c17c7e2f3daed8339d68e97a05107989bada68
Marco A Benatto <benatto@mandriva.com.br>
2013-01-15 17:04:36 -0200

    initied io part and added old (not-functional) driver

--- egistec0603.h
+++ egistec0603.h
@@ -6,7 +6,20 @@
 static int usb_egistec0603_probe(struct usb_interface*, const struct usb_device_id*);
 
 struct usb_egistec0603{
-	struct input_dev *dev;
+	struct input_dev *udev;
+	struct usb_interface *interface;
+
+	size_t bulk_in_size;
+	__u8   bulk_in_endpointaddr;
+	__u8   bulk_out_endpointaddr;
+
+	int errors;
+	int open_count; /*count of openers*/	
+	spinlock_t err_lock; /*error lock*/
+	struct kref kref; /*record the opened times*/
+	struct mutex io_mutex; /*mutex to sync I/O with disconnect*/
+
+	struct mutex SCSI_mutex
 };
 
 
